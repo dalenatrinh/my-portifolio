9964c61f62b9f506ec703cb753df8de5841cc041
Dalena <79627422+dalenatrinh@users.noreply.github.com>
2023-07-23 22:16:36 +0200

    doing fish animation

---
+++ Desktop/job-praktik/portefolio/vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import FishTank from './components/FishTank.vue'
+import MenuBar from './components/MenuBar.vue'
+</script>
+
+<template>
+  <header>
+    <h2 class="dalena mystery" id="hiDalena">Hello Dalena</h2>
+
+   <MenuBar />
+  </header>
+  <main>
+    <FishTank />
+  </main>
+</template>
+
+<style src="./assets/main.css" lang="scss">
+
+
+
+
+</style>
---
+++ Desktop/job-praktik/portefolio/vue-project/src/components/MenuBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <li>Logo</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+       
+    </div>
+</template>
+
+<style scoped>
+li {
+    list-style-type: none;
+}
+
+div {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    width: 70%;
+    margin: auto;
+    padding-top: 50px;
+}
+</style>
